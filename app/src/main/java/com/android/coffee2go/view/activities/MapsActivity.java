6edc727559785a4344490c2f286526505b426b99
Alex-Teodosiu <61668352+Alex-Teodosiu@users.noreply.github.com>
2021-11-28 21:02:02 +0100

    Maps update

--- app/src/main/java/com/android/coffee2go/view/activities/MapsActivity.java
+++ app/src/main/java/com/android/coffee2go/view/activities/MapsActivity.java
@@ -1,17 +1,18 @@
-package com.example.googlemaps;
-
-import androidx.fragment.app.FragmentActivity;
+package com.android.coffee2go.view.activities;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
 import com.android.coffee2go.R;
+import com.android.coffee2go.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.googlemaps.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -28,33 +29,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        if (mapFragment == null) throw new AssertionError();
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        //Initialize the map
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        // A few coffee shop coordinates
         LatLng seveneleven = new LatLng(55.862954, 9.836691);
         LatLng cafeoxygen = new LatLng(55.862946, 9.848320);
         LatLng cafeovivaldi = new LatLng(55.862195, 9.847103);
         LatLng cafesocialistik = new LatLng(55.863829, 9.848038);
         LatLng cafehimebla = new LatLng(55.851925, 9.830076);
+
+        //Adding the coffee shop coordinates to the map as markers
         mMap.addMarker(new MarkerOptions().position(seveneleven).title("Marker in 7/11 horsens train station"));
         mMap.addMarker(new MarkerOptions().position(cafeoxygen).title("Marker in cafe oxygen"));
         mMap.addMarker(new MarkerOptions().position(cafeovivaldi).title("cafe vivaldi"));
         mMap.addMarker(new MarkerOptions().position(cafesocialistik).title("socialist coffee club"));
         mMap.addMarker(new MarkerOptions().position(cafehimebla).title("Mcafe imebla"));
+
+        //Zoom the map to orsens
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cafeoxygen, 15f));
     }
 }
