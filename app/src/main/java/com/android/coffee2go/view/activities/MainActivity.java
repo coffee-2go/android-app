57e37d5f4965d55f7cc631eeee42a9b700ce4d5d
asterixcode <lk.saldanha@gmail.com>
2021-11-29 13:54:23 +0100

    feat: edit profile activity design

--- app/src/main/java/com/android/coffee2go/view/activities/MainActivity.java
+++ app/src/main/java/com/android/coffee2go/view/activities/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         // config toolbar
         Toolbar toolbar = findViewById(R.id.toolbarMain);
-        toolbar.setTitle("Coffeee2go");
+        toolbar.setTitle("Coffee2go");
         setSupportActionBar(toolbar);
 
         // config object
