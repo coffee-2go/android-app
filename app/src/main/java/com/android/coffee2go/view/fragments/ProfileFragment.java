3828b9c9329cd1e1b42b88125d26ec1ce5e764a8
asterixcode <lk.saldanha@gmail.com>
2021-12-01 22:11:40 +0100

    feat: show username on profile fragment

--- app/src/main/java/com/android/coffee2go/view/fragments/ProfileFragment.java
+++ app/src/main/java/com/android/coffee2go/view/fragments/ProfileFragment.java
@@ -3,34 +3,24 @@ package com.android.coffee2go.view.fragments;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
 import com.android.coffee2go.R;
-import com.android.coffee2go.data.AccountDAO;
 import com.android.coffee2go.helper.AccountFirebase;
 import com.android.coffee2go.helper.ConfigFirebase;
 import com.android.coffee2go.models.Account;
 import com.android.coffee2go.view.activities.EditProfileActivity;
 import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.HashMap;
-
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileFragment extends Fragment {
@@ -49,7 +39,11 @@ public class ProfileFragment extends Fragment {
     private TextView textUsername;
     private Button buttonEditProfile;
     private Account loggedAccount;
-    private DatabaseReference reference;
+
+    private DatabaseReference firebaseRef;
+    private DatabaseReference accountsRef;
+    private DatabaseReference loggedAccountRef;
+    private ValueEventListener valueEventListenerAccount;
 
 
     public ProfileFragment() {
@@ -91,16 +85,13 @@ public class ProfileFragment extends Fragment {
 
         // initial config
         loggedAccount = AccountFirebase.getDataLoggedAccount();
+        firebaseRef = ConfigFirebase.getDatabaseReference();
+        accountsRef = firebaseRef.child("accounts");
 
         // config components
         initComponents(view);
-//        Account account = AccountDAO.getInstance().getAccount(loggedAccount.getId());
-//        Log.i("PROFILE FRAGMENT",account.toString());
-//        textUsername.setText(account.getUsername());
-//        Log.i("PROFILE FRAGMENT",account.toString());
 
-
-        // open edit perfil
+        // open edit profile
         buttonEditProfile.setOnClickListener(view1 -> {
             Intent intent = new Intent(getActivity(), EditProfileActivity.class);
             startActivity(intent);
@@ -116,9 +107,26 @@ public class ProfileFragment extends Fragment {
         buttonEditProfile = view.findViewById(R.id.profile_buttonEditProfile);
     }
 
+    private void getDataLoggedAccount() {
+        loggedAccountRef = accountsRef.child(loggedAccount.getId());
+        valueEventListenerAccount = loggedAccountRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Account account = snapshot.getValue( Account.class );
+
+                String username = String.valueOf( account.getUsername() );
+
+                textUsername.setText(username);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
 
     private void retrieveAccountPicture() {
-
         loggedAccount = AccountFirebase.getDataLoggedAccount();
 
         // retrieve account picture
@@ -134,9 +142,17 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        //AccountDAO.getInstance().getAccount(loggedAccount.getId());
+
+        // retrieve data logged account
+        getDataLoggedAccount();
 
         // retrieve account picture
         retrieveAccountPicture();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        loggedAccountRef.removeEventListener(valueEventListenerAccount);
     }
 }
