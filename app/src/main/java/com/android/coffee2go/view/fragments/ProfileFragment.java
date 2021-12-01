db27086d599e0efb54dc1b05d6fd36c8103c2a79
asterixcode <lk.saldanha@gmail.com>
2021-12-01 19:40:38 +0100

    feat: permissions and profile update

--- app/src/main/java/com/android/coffee2go/view/fragments/ProfileFragment.java
+++ app/src/main/java/com/android/coffee2go/view/fragments/ProfileFragment.java
@@ -1,10 +1,13 @@
 package com.android.coffee2go.view.fragments;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +16,20 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.android.coffee2go.R;
+import com.android.coffee2go.data.AccountDAO;
+import com.android.coffee2go.helper.AccountFirebase;
+import com.android.coffee2go.helper.ConfigFirebase;
+import com.android.coffee2go.models.Account;
 import com.android.coffee2go.view.activities.EditProfileActivity;
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -32,6 +48,9 @@ public class ProfileFragment extends Fragment {
     private CircleImageView imageProfile;
     private TextView textUsername;
     private Button buttonEditProfile;
+    private Account loggedAccount;
+    private DatabaseReference reference;
+
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -70,11 +89,16 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        // initial config
+        loggedAccount = AccountFirebase.getDataLoggedAccount();
+
         // config components
-        progressBar = view.findViewById(R.id.profile_progressBar);
-        imageProfile = view.findViewById(R.id.profile_imageView);
-        textUsername = view.findViewById(R.id.profile_textUsername);
-        buttonEditProfile = view.findViewById(R.id.profile_buttonEditProfile);
+        initComponents(view);
+//        Account account = AccountDAO.getInstance().getAccount(loggedAccount.getId());
+//        Log.i("PROFILE FRAGMENT",account.toString());
+//        textUsername.setText(account.getUsername());
+//        Log.i("PROFILE FRAGMENT",account.toString());
+
 
         // open edit perfil
         buttonEditProfile.setOnClickListener(view1 -> {
@@ -83,5 +107,36 @@ public class ProfileFragment extends Fragment {
         });
 
         return view;
+    }
+
+    public void initComponents(View view) {
+        progressBar = view.findViewById(R.id.profile_progressBar);
+        imageProfile = view.findViewById(R.id.profile_imageView);
+        textUsername = view.findViewById(R.id.profile_textUsername);
+        buttonEditProfile = view.findViewById(R.id.profile_buttonEditProfile);
+    }
+
+
+    private void retrieveAccountPicture() {
+
+        loggedAccount = AccountFirebase.getDataLoggedAccount();
+
+        // retrieve account picture
+        String urlPicture = loggedAccount.getUrlPicture();
+        if ( urlPicture != null ) {
+            Uri url = Uri.parse( urlPicture );
+            Glide.with(getActivity())
+                    .load( url )
+                    .into( imageProfile );
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        //AccountDAO.getInstance().getAccount(loggedAccount.getId());
+
+        // retrieve account picture
+        retrieveAccountPicture();
     }
 }
