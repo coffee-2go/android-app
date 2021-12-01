cd5bdc8c392383eb031ddd2bca6ee2ea0aeff612
asterixcode <lk.saldanha@gmail.com>
2021-12-01 10:02:15 +0100

    fix: update main activity

--- app/src/main/java/com/android/coffee2go/view/activities/MainActivity.java
+++ app/src/main/java/com/android/coffee2go/view/activities/MainActivity.java
@@ -5,22 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.android.coffee2go.R;
-import com.android.coffee2go.helper.FirebaseConfig;
-import com.android.coffee2go.view.fragments.CartFragment;
-import com.android.coffee2go.view.fragments.LocationFragment;
+import com.android.coffee2go.helper.ConfigFirebase;
 import com.android.coffee2go.view.fragments.MenuFragment;
-import com.android.coffee2go.view.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,15 +36,11 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("Coffee2go");
         setSupportActionBar(toolbar);
 
-        // config object
-        auth = FirebaseConfig.getFirebaseAuth();
+        // config auth object (for sign out option)
+        auth = ConfigFirebase.getFirebaseAuth();
 
         // config bottom navigation
         configBottomNavigationView();
-        // when first opening, open MenuFragment
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.viewPager, new MenuFragment()).commit();
 
     }
 
@@ -55,35 +50,23 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         // bottomNavigationView.setLabelVisibilityMode(NavigationBarView.LABEL_VISIBILITY_UNLABELED);
 
-        // enable navigation
-        enableNavigation(bottomNavigationView);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration
+                .Builder(R.id.menuFragment,R.id.cartFragment,R.id.locationFragment,R.id.profileFragment).build();
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.mainActivity_fragmentContainerView_NavHostFragment);
+
+        if (navHostFragment != null) {
+
+            NavController navController = navHostFragment.getNavController();
+
+            // Setup NavigationUI here
+            NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        }
     }
 
-    public void enableNavigation(BottomNavigationView view){
-        view.setOnItemSelectedListener(item -> {
 
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-            int itemId = item.getItemId();
-            if (itemId == R.id.menuFragment) {
-                fragmentTransaction.replace(R.id.viewPager, new MenuFragment()).commit();
-                return true;
-            } else if (itemId == R.id.cartFragment) {
-                fragmentTransaction.replace(R.id.viewPager, new CartFragment()).commit();
-                return true;
-            } else if (itemId == R.id.locationFragment) {
-                fragmentTransaction.replace(R.id.viewPager, new LocationFragment()).commit();
-                return true;
-            } else if (itemId == R.id.profileFragment) {
-                fragmentTransaction.replace(R.id.viewPager, new ProfileFragment()).commit();
-                return true;
-            }
-            // if no fragment is committed, then return false
-            return false;
-        });
-    }
-
+    // --- top bar options menu ---
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -96,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
+        // if item = logout
         if (item.getItemId() == R.id.menu_logout) {
             signOut();
             startActivity(new Intent(getApplicationContext(), LoginActivity.class));
